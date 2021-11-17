0089e5453edf511d6a8ebc3dd9a75961c4840987
sevoon0909 <84161434+sevoon0909@users.noreply.github.com>
2021-11-17 21:24:08 +0900

    pug 추가

--- src/server.js
+++ src/server.js
@@ -7,12 +7,19 @@ import userRouter from "./routers/userRouter";
 const port = 4000;
 
 const app = express();
-const logger = morgan("dev")
-app.use("/",logger);
+const logger = morgan("dev");
 
-app.use("/",()=>console.log("@"));
+app.set("view engine","pug");
+app.set("views", process.cwd() + '/src/views');
+app.use(logger);
+
+console.log(app);
+
+
+app.use("/",globalRouter);
 app.use("/users",userRouter);
 app.use("/videos",videoRouter);
+
 
 
 app.listen(port,() => console.log("Server listenning on port 4000"))
